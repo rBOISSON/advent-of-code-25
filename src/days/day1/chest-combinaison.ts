78f8495b2c279544ef3bc65bb3f9f254d142690d
romain-ES <romain@edusign.fr>
2025-12-02 21:04:25 +0100

    resolve first challenge

--- src/days/day1/chest-combinaison.ts
+++ src/days/day1/chest-combinaison.ts
@@ -1,12 +1,51 @@
 import path from 'path';
 import { getFileContent } from '../../helpers';
 
-export const findChestCombinaison = () => {
-    const inputData = getFileContent(`${path.dirname(__filename)}/input-day-1.txt`);
+const MODULO = 100;
 
-    console.log("inputdata", inputData);
+export const findChestCombinaison = () => {
+    const inputData = getFileContent(`${path.dirname(__filename)}/input-day-1.txt`).filter(data => data.length > 0);
+
+    let count = 0;
+    let currentChestValue = 50;
+
+    for(const input of inputData) {
+        console.log("input", input);
+        currentChestValue = applyCode(currentChestValue, input);
+        if (currentChestValue === 0) {
+            count++;
+        }
+    }
+
+    console.log("result", count);
 }
 
-const modulo = (n, mod) => {
-    return ((n % mod) + mod) % mod;
+const applyCode = (currentChestValue: number, valueToApply: string) : number => {
+    const { rotation, rotationValue } = readRotationInstruction(valueToApply);
+    let res = currentChestValue; 
+    if (rotation == 'L') {
+        res -= rotationValue;
+    } else {
+        res += rotationValue;
+    }
+    return modulo(res);
+}
+
+const readRotationInstruction = (rotationInstruction: string): {rotation: 'L' | 'R', rotationValue: number} => {
+
+    const rotation = rotationInstruction.slice(0, 1); 
+    const rotationValue = parseInt(rotationInstruction.slice(1, rotationInstruction.length));
+    
+    if (rotation != 'L' && rotation != 'R') {
+        throw new Error(`unexpected first letter ${rotation}`);
+    }
+
+    return { 
+        rotation,
+        rotationValue 
+    }
+}
+
+const modulo = (n) => {
+    return ((n % MODULO) + MODULO) % MODULO;
 };
