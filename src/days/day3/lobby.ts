c616d69d85c6376bdfae199eabb210b3dc14df78
romain.boisson5 <romain.boisson5@gmail.com>
2025-12-27 01:05:56 +0100

    create nice function to handle day 3, even make first part deprecated

--- src/days/day3/lobby.ts
+++ src/days/day3/lobby.ts
@@ -10,29 +10,75 @@ export const getPowerBanksValue = () => {
 
   let res = 0;
 
-  for (const powerBank of powerbanks) {
-    const highestPowerBankValue = getHigestBankValue(powerBank);
+  for (const powerbank of powerbanks) {
+    const highestPowerBankValue = getHigestTwoDigitsBankValue(powerbank);
     console.log("highest power bank value", highestPowerBankValue);
     res += parseInt(highestPowerBankValue);
   }
 
-  console.log("result simple power bankls", res);
+  console.log("result simple power banks", res);
+
+  let resPartTwo = 0;
+
+  for (const powerbank of powerbanks) {
+    const highestPowerbankValue = getHighestBankValue(powerbank, 12);
+    console.log("highest power bank value", highestPowerbankValue);
+    resPartTwo += parseInt(highestPowerbankValue);
+  }
+
+  console.log("result part two power banks", resPartTwo);
 };
 
-const getHigestBankValue = (powerBank: string): string => {
+const getHighestBankValue = (powerbank: string, digits: number): string => {
+  const starter = findHighest(powerbank, 0, powerbank.length - (digits - 1));
+  const highestValue: string[] = [starter.highest];
+  let currentHighestIndex: number = starter.index;
+
+  for (let i = highestValue.length; i < digits; i++) {
+    const max = powerbank.length - (digits - i - 1);
+    const currentHigest = findHighest(powerbank, currentHighestIndex + 1, max);
+    currentHighestIndex = currentHigest.index;
+    highestValue.push(currentHigest.highest);
+  }
+
+  return highestValue.join("");
+};
+
+const findHighest = (
+  powerbank: string,
+  start: number,
+  end: number
+): { index: number; highest: string } => {
   let firstHighest: string = "0";
   let indexHighest: number = 0;
-  for (let i = 0; i < powerBank.length - 1; i++) {
-    if (powerBank[i] > firstHighest) {
-      firstHighest = powerBank[i];
+
+  for (let i = start; i < end; i++) {
+    if (powerbank[i] > firstHighest) {
+      firstHighest = powerbank[i];
+      indexHighest = i;
+    }
+  }
+
+  return {
+    index: indexHighest,
+    highest: firstHighest,
+  };
+};
+
+const getHigestTwoDigitsBankValue = (powerbank: string): string => {
+  let firstHighest: string = "0";
+  let indexHighest: number = 0;
+  for (let i = 0; i < powerbank.length - 1; i++) {
+    if (powerbank[i] > firstHighest) {
+      firstHighest = powerbank[i];
       indexHighest = i;
     }
   }
 
   let secondHighest = "0";
-  for (let i = indexHighest + 1; i < powerBank.length; i++) {
-    if (powerBank[i] > secondHighest) {
-      secondHighest = powerBank[i];
+  for (let i = indexHighest + 1; i < powerbank.length; i++) {
+    if (powerbank[i] > secondHighest) {
+      secondHighest = powerbank[i];
     }
   }
 
